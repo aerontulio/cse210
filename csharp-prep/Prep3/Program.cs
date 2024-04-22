ae38d6950fd39d08db9e262fb76b941f1b43ffb6
aerontulio <tulioaeronjake1@gmail.com>
2024-04-22 16:35:23 +0800

    submitting prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        while (true)
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 100);
+            
+            while (true)
+            {
+                Console.Write("What is your guess? ");
+                string valueFromUser = Console.ReadLine();
+                int guess = int.Parse(valueFromUser);
+
+                if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    break;
+                }
+            }
+
+            Console.Write("Do you want to play again? (yes/no) ");
+            string again = Console.ReadLine();
+            if (again != "yes")
+            {
+                break;
+            }
+
+        }
+        
     }
 }
