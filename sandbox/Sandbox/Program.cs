68b3913d211e27d762246902fe25570a7cbe6cd1
aerontulio <tulioaeronjake1@gmail.com>
2024-04-26 15:01:07 +0800

    practicing c#

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Transactions;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Sandbox World!");
+    {  
+        int i = 0;
+        int number = 5;
+
+        while (i < 3)
+        {
+            Console.Write("Guess the number from 1 to 10: ");
+            int guess = int.Parse(Console.ReadLine());
+            if (guess == number)
+            {
+                Console.WriteLine("You're right");
+                break;
+            }
+            i++;
+            Console.WriteLine("Try Again");
+        }
+
     }
-}
+} 
